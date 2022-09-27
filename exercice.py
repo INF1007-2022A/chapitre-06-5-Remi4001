1aec3b6818a5acf327f801e7dd731a5144b83d9a
Remi4001 <remilabelle@outlook.com>
2022-09-27 19:27:46 -0400

    Compléter check_brackets

--- exercice.py
+++ exercice.py
@@ -1,90 +1,108 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from collections import deque
+
 
 def check_brackets(text, brackets):
-	return False
+    opening = dict(zip(brackets[0::2], brackets[1::2]))
+    closing = dict(zip(brackets[1::2], brackets[0::2]))
+
+    bracket_stack = deque()
+
+    for t in text:
+        if t in opening:
+            bracket_stack.append(opening[t])
+        elif t in closing:
+            if bracket_stack and bracket_stack[-1] == t:
+                bracket_stack.pop()
+            else:
+                return False
+
+    return not bracket_stack
+
 
 def remove_comments(full_text, comment_start, comment_end):
-	return ""
+    return ""
+
 
 def get_tag_prefix(text, opening_tags, closing_tags):
-	return (None, None)
+    return (None, None)
+
 
 def check_tags(full_text, tag_names, comment_tags):
-	return False
+    return False
 
 
 if __name__ == "__main__":
-	brackets = ("(", ")", "{", "}")
-	yeet = "(yeet){yeet}"
-	yeeet = "({yeet})"
-	yeeeet = "({yeet)}"
-	yeeeeet = "(yeet"
-	print(check_brackets(yeet, brackets))
-	print(check_brackets(yeeet, brackets))
-	print(check_brackets(yeeeet, brackets))
-	print(check_brackets(yeeeeet, brackets))
-	print()
+    brackets = ("(", ")", "{", "}")
+    yeet = "(yeet){yeet}"
+    yeeet = "({yeet})"
+    yeeeet = "({yeet)}"
+    yeeeeet = "(yeet"
+    print(check_brackets(yeet, brackets))
+    print(check_brackets(yeeet, brackets))
+    print(check_brackets(yeeeet, brackets))
+    print(check_brackets(yeeeeet, brackets))
+    print()
 
-	spam = "Hello, /* OOGAH BOOGAH */world!"
-	eggs = "Hello, /* OOGAH BOOGAH world!"
-	parrot = "Hello, OOGAH BOOGAH*/ world!"
-	print(remove_comments(spam, "/*", "*/"))
-	print(remove_comments(eggs, "/*", "*/"))
-	print(remove_comments(parrot, "/*", "*/"))
-	print()
+    spam = "Hello, /* OOGAH BOOGAH */world!"
+    eggs = "Hello, /* OOGAH BOOGAH world!"
+    parrot = "Hello, OOGAH BOOGAH*/ world!"
+    print(remove_comments(spam, "/*", "*/"))
+    print(remove_comments(eggs, "/*", "*/"))
+    print(remove_comments(parrot, "/*", "*/"))
+    print()
 
-	otags = ("<head>", "<body>", "<h1>")
-	ctags = ("</head>", "</body>", "</h1>")
-	print(get_tag_prefix("<body><h1>Hello!</h1></body>", otags, ctags))
-	print(get_tag_prefix("<h1>Hello!</h1></body>", otags, ctags))
-	print(get_tag_prefix("Hello!</h1></body>", otags, ctags))
-	print(get_tag_prefix("</h1></body>", otags, ctags))
-	print(get_tag_prefix("</body>", otags, ctags))
-	print()
+    otags = ("<head>", "<body>", "<h1>")
+    ctags = ("</head>", "</body>", "</h1>")
+    print(get_tag_prefix("<body><h1>Hello!</h1></body>", otags, ctags))
+    print(get_tag_prefix("<h1>Hello!</h1></body>", otags, ctags))
+    print(get_tag_prefix("Hello!</h1></body>", otags, ctags))
+    print(get_tag_prefix("</h1></body>", otags, ctags))
+    print(get_tag_prefix("</body>", otags, ctags))
+    print()
 
-	spam = (
-		"<html>"
-		"  <head>"
-		"    <title>"
-		"      <!-- Ici j'ai écrit qqch -->"
-		"      Example"
-		"    </title>"
-		"  </head>"
-		"  <body>"
-		"    <h1>Hello, world</h1>"
-		"    <!-- Les tags vides sont ignorés -->"
-		"    <br>"
-		"    <h1/>"
-		"  </body>"
-		"</html>"
-	)
-	eggs = (
-		"<html>"
-		"  <head>"
-		"    <title>"
-		"      <!-- Ici j'ai écrit qqch -->"
-		"      Example"
-		"    <!-- Il manque un end tag"
-		"    </title>-->"
-		"  </head>"
-		"</html>"
-	)
-	parrot = (
-		"<html>"
-		"  <head>"
-		"    <title>"
-		"      Commentaire mal formé -->"
-		"      Example"
-		"    </title>"
-		"  </head>"
-		"</html>"
-	)
-	tags = ("html", "head", "title", "body", "h1")
-	comment_tags = ("<!--", "-->")
-	print(check_tags(spam, tags, comment_tags))
-	print(check_tags(eggs, tags, comment_tags))
-	print(check_tags(parrot, tags, comment_tags))
-	print()
-
+    spam = (
+        "<html>"
+        "  <head>"
+        "    <title>"
+        "      <!-- Ici j'ai écrit qqch -->"
+        "      Example"
+        "    </title>"
+        "  </head>"
+        "  <body>"
+        "    <h1>Hello, world</h1>"
+        "    <!-- Les tags vides sont ignorés -->"
+        "    <br>"
+        "    <h1/>"
+        "  </body>"
+        "</html>"
+    )
+    eggs = (
+        "<html>"
+        "  <head>"
+        "    <title>"
+        "      <!-- Ici j'ai écrit qqch -->"
+        "      Example"
+        "    <!-- Il manque un end tag"
+        "    </title>-->"
+        "  </head>"
+        "</html>"
+    )
+    parrot = (
+        "<html>"
+        "  <head>"
+        "    <title>"
+        "      Commentaire mal formé -->"
+        "      Example"
+        "    </title>"
+        "  </head>"
+        "</html>"
+    )
+    tags = ("html", "head", "title", "body", "h1")
+    comment_tags = ("<!--", "-->")
+    print(check_tags(spam, tags, comment_tags))
+    print(check_tags(eggs, tags, comment_tags))
+    print(check_tags(parrot, tags, comment_tags))
+    print()
