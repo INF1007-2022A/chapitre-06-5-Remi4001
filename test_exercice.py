55d29a5a901a779a30f0dff122c68668640ad83b
Remi4001 <remilabelle@outlook.com>
2022-09-28 17:44:08 -0400

    Ajout de test_check_brackets()

--- test_exercice.py
+++ test_exercice.py
@@ -9,14 +9,36 @@ import exercice
 
 
 class TestExercice(unittest.TestCase):
-	def test_its_all_good(self):
-		self.assertTrue(True)
+    def test_check_brackets(self):
+        brackets = ("(", ")", "{", "}")
+        values = [
+            "(yeet){yeet}",
+            "({yeet})",
+            "({yeet)}",
+            "(yeet",
+            "yeet}",
+        ]
+        expected = [
+            True,
+            True,
+            False,
+            False,
+            False,
+        ]
+
+        output = [exercice.check_brackets(v, brackets) for v in values]
+        self.assertListEqual(
+            output,
+            expected,
+            "Mauvaise vérification des parenthèses et accolades"
+        )
+
 
 if __name__ == '__main__':
-	if not os.path.exists('logs'):
-		os.mkdir('logs')
-	with open('logs/tests_results.txt', 'w') as f:
-		loader = unittest.TestLoader()
-		suite = loader.loadTestsFromModule(sys.modules[__name__])
-		unittest.TextTestRunner(f, verbosity=2).run(suite)
-	print(open('logs/tests_results.txt', 'r').read())
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    with open('logs/tests_results.txt', 'w') as f:
+        loader = unittest.TestLoader()
+        suite = loader.loadTestsFromModule(sys.modules[__name__])
+        unittest.TextTestRunner(f, verbosity=2).run(suite)
+    print(open('logs/tests_results.txt', 'r').read())
